58c870d32947ebca4192b6451f988a25a47df709
David Espinoza <davidespinoza@att.net>
2019-05-07 05:05:32 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+  return "Welcome, " + name + "." + " You are number " + katzDeliLine.length + " in line.";
+}
+
+function nowServing(katzDeliLine){
+  katzDeliLine.pop(katzDeliLine[0]);
+  if(katzDeliLine.length == 0){
+    return "There is nobody waiting to be served!";
+  }
+}
