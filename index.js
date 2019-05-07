92445423dbdf84c8869c2d15239bd9117f01e3fa
David Espinoza <davidespinoza@att.net>
2019-05-07 05:16:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ function takeANumber(katzDeliLine, name){
 }
 
 function nowServing(katzDeliLine){
-  return katzDeliLine[0];
+  if(katzDeliLine.length == 0){
+    return "There is nobody waiting to be served!";
+  }
 }
